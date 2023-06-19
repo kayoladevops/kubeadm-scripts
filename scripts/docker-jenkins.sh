e0f68a965103eb52d2bbc1a2ff2eb376149079db
kayoladevops <kayoladevops@yahoo.com>
2023-06-19 14:22:33 -0400

    new files

--- scripts/docker-jenkins.sh
+++ scripts/docker-jenkins.sh
@@ -11,3 +11,17 @@ sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo usermod -aG docker ubuntu
+
+# installing Jenkins and adding it to docker grp 
+
+sudo apt update
+sudo apt install openjdk-8-jdk openjdk-11-jdk -y
+sudo apt install maven -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo usermod -aG docker jenkins
