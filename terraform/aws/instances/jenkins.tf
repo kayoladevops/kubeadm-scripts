895070d102b05dd40c1ca0d2719a5b89a15f54bf
kayoladevops <kayoladevops@yahoo.com>
2023-06-21 05:07:44 -0400

    join

--- terraform/aws/instances/jenkins.tf
+++ terraform/aws/instances/jenkins.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "jenkins-docker-inst" {
   ami               = "ami-053b0d53c279acc90"
   instance_type     = "t2.medium"
   key_name          = "docker"
-  subnet_id         = ["subnet-06be1022cf7218354"]
+  subnet_id         = "subnet-06be1022cf7218354"
   security_groups   = ["sg-0f4e4ea1d8907c730"]
   availability_zone = "us-east-1a"
   
